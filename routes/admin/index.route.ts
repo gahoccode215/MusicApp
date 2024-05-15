60c8740d41cea1257eeb16ed76d99faf1cdcd9aa
Gà Học Code <dominhit215@gmail.com>
2024-05-15 09:54:58 +0700

    admin-songs

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/system";
 import { topicRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 
 const adminRoutes = (app: Express): void => {
 
@@ -10,6 +11,8 @@ const adminRoutes = (app: Express): void => {
     app.use(`/${prefixAdmin}/dashboard`, dashboardRoutes);
 
     app.use(`/${prefixAdmin}/topics`, topicRoutes);
+
+    app.use(`/${prefixAdmin}/songs`, songRoutes);
 };
 
 export default adminRoutes;
